b524123737755079b2ed7696d73fb4198892e482
sachinsp14 <sachinsp14@gmail.com>
2016-11-19 11:29:07 -0800

    Add files via upload

---
+++ sqrtclient.c
@@ -0,0 +1,156 @@
+/* CmpE 207  Lab Assignment #1
+/* Topic : Client and server software design using sockets
+/* Author: Group 8
+/* This TCP Client passes an integer to the server. Server takes this integer, computes the square root and send the result back to client.
+Client is reading the result and prints the initial value and the square root value. */
+
+#include <stdio.h>
+#define _USE_BSD 1
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <time.h>
+
+#define BUFSIZE 64
+
+#ifndef INADDR_NONE
+#define INADDR_NONE 0xffffffff
+#endif
+
+extern int errno;
+
+int errexit(const char *format,...);
+int connectTCP(const char *service,const char *host);
+int connectsock(const char *service,const char *host,const char *transport);
+
+int main(int argc,char *argv[])
+{
+char *host;
+char recvmsg[100];
+char *number;
+double result;
+char *service="echo";
+
+int sock,n;
+
+switch(argc)
+{
+case 1:
+host="localhost";
+break;
+
+case 2:
+host="localhost";
+break;
+
+case 3:
+number=argv[2];
+host=argv[1];
+break;
+
+default:printf("Error in reading arguments\n");
+exit(1);
+}
+
+result=atof(number);
+
+/* Establishes connectivity with the server by choosing TCP protocol. Returns a positive Integer on connecting. */
+
+sock=connectTCP(service,host);
+
+/* On establishing the connection, the integer stored in number is sent to the server who's length is the length of number+1 */
+
+write(sock,number,strlen(number)+1);
+
+/* The client receives the square root result back from the server and is stored in recvmsg and has a length of 100 */
+
+n=read(sock,recvmsg,100);
+
+/* Printing the entered number */
+
+if(n<0)
+{
+printf("reading failed\n");
+}
+
+printf("Entered number is : %s\n",number);
+
+/* Printing the result received from server. */
+
+if(result>=0)
+{
+ printf("Square root of given number : %s\n",recvmsg);
+}
+else if(result<0)
+{
+  printf("%s\n",recvmsg);
+}
+exit(0);
+
+}
+
+/* connectsock - allocate & connect a socket using TCP or UDP */
+
+int connectTCP(const char *service,const char *host)
+{
+return connectsock(service,host,"tcp");
+}
+
+/*
+* Arguments:
+* host - name of host to which connection is desired
+* service - service associated with the desired port
+* transport - name of transport protocol to use ("tcp" or "udp")
+*/
+
+int connectsock(const char *service,const char *host,const char *transport)
+{
+struct sockaddr_in sin;
+int sock,type;
+
+memset(&sin,0,sizeof(sin));
+sin.sin_family=AF_INET;
+
+sin.sin_port=htons(8000);
+
+inet_pton(AF_INET,host,&(sin.sin_addr));
+
+/* Use protocol to choose a socket type */
+
+if(strcmp(transport,"udp")==0)
+type=SOCK_DGRAM;
+else
+type=SOCK_STREAM;
+
+ /* Allocate a socket */
+
+sock=socket(AF_INET,type,0);
+
+if(sock<0)
+errexit("Error in creating socket : %s\n",strerror(errno));
+
+/* Connect the socket */
+
+if((connect(sock,(struct sockaddr *) &sin,sizeof(sin)))<0)
+errexit("Error in connection %s.%s: %s\n",host,service,strerror(errno));
+
+return sock;
+}
+
+int errexit(const char* format,...)
+{
+va_list args;
+
+va_start(args,format);
+vfprintf(stderr,format,args);
+va_end(args);
+exit(1);
+}
+
---
+++ sqrtserver.c
@@ -0,0 +1,148 @@
+/* CmpE 207  Lab Assignment #1
+/* Topic : Client and server software design using sockets
+/* Author: Group 8
+/* This TCP Server accepts an integer from the Client. Server takes this integer, computes the square root and send the result back to client.
+Client is reading the result and prints the initial value and the square root value. */
+
+#include <stdio.h>
+#define _USE_BSD 1
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <time.h>
+#include <math.h>
+
+int connectTCP(const char *service);
+int connectsock(const char *service,const char *transport);
+
+int main(char argc,char *argv[])
+{
+
+char message[100];
+char *service="echo";
+char errmsg[1000]="Entered number is negative.Please enter positive number";
+
+int acceptsock,sock;
+struct sockaddr_in sin;
+
+sock=connectTCP(service);
+
+/* Accepts the connection from the client. */
+
+acceptsock=accept(sock,(struct sockaddr*)NULL,NULL);
+
+if(acceptsock<0)
+{
+printf("error in accepting\n");
+exit(0);
+}
+
+/* Reads the data from Client and the data is stored in message which has a length of 100. */
+
+read(acceptsock,message,100);
+double number;
+
+/* Converting the data type to double */
+
+number =atof(message);
+double result;
+
+/* Once the socket connection is accepted, the result is calculated and is sent back to the Client using write. */
+
+if(number<0)
+{
+write(acceptsock,errmsg,1000);
+}
+else if(number>=0)
+{
+result=sqrt(number);
+printf("Number recieved from client:%f\n",number);
+printf("Square root of the number:%f\n",result);
+sprintf(message,"%f",result);
+write(acceptsock,message,strlen(message)+1);
+}
+
+/* Once the result is sent back, the acceptsocket is closed. */
+
+close(acceptsock);
+return 0;
+}
+
+int connectsock(const char *service,const char *transport)
+{
+struct sockaddr_in server;
+
+int serversock,type,acceptsock,number;
+
+memset(&server,0,sizeof(server));
+
+server.sin_addr.s_addr=htons(INADDR_ANY);
+server.sin_family=AF_INET;
+server.sin_port=htons(8000);
+
+/* Use protocol to choose a socket type */
+
+if(strcmp(transport,"udp")==0)
+{
+type=SOCK_DGRAM;
+}
+else
+{
+type=SOCK_STREAM;
+}
+
+/* Allocate a socket */
+
+serversock=socket(AF_INET,type,0);
+
+if(serversock<0)
+{
+printf("Cant create socket\n");
+exit(0);
+}
+
+number=1;
+
+if(setsockopt(serversock,SOL_SOCKET,SO_REUSEPORT,(const char*)&number,sizeof(number))<0)
+{
+printf("setsockopt(SO_REUSEPORT) failed\n");
+exit(0);
+}
+
+/* Bind Call : Adds details from serversock to socket descriptor */
+
+int b;
+b=bind(serversock,(struct sockaddr*)&server,sizeof(server));
+
+if(b<0)
+{
+printf("Binding error\n");
+exit(0);
+}
+
+/* Listen Call : Allows 10 connections at the socket to wait in queue  */
+
+int listeningsock;
+listeningsock=listen(serversock,10);
+if(listeningsock<0)
+{
+printf("Error in listening\n");
+exit(0);
+}
+
+return serversock;
+}
+
+
+int connectTCP(const char *service)
+{
+ return connectsock(service,"tcp");
+}
+
